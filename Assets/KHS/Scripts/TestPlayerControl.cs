517d2f73d4f1f4fcf67531bffe689144903374c0
dr7007 <dr7007@naver.com>
2025-01-24 18:06:58 +0900

    가드 AI Prototype 작업본 업데이트

--- Assets/KHS/Scripts/TestPlayerControl.cs
+++ Assets/KHS/Scripts/TestPlayerControl.cs
@@ -3,23 +3,16 @@ using UnityEngine;
 public class TestPlayerControl : MonoBehaviour
 {
     public Transform mainCam;
-    public Transform target; // 카메라가 따라다닐 플레이어
+    public Transform npcTr;
 
-    public float distance = 3f; // 플레이어와 카메라 간 거리
-    public float height = 1.5f; // 카메라 높이
-    public float sensitivity = 3f; // 마우스 감도
-    public float cameraSpeed = 10f; // 카메라 부드러운 움직임 속도
     public float moveSpeed = 4.0f;
     public float rotationSpeed = 10f;
 
     private CharacterController characterController;
     private Vector3 moveDirection;
-    private float yaw; // 좌우 회전 (Y축 기준)
-    private float pitch; // 상하 회전 (X축 기준)
 
     private void Awake()
     {
-        target = transform;
         mainCam = GetComponentInChildren<Camera>().transform.parent;
         characterController = GetComponent<CharacterController>();
     }
@@ -32,7 +25,7 @@ public class TestPlayerControl : MonoBehaviour
     private void Update()
     {
         PlayerBasicMove();
-        PlayerCamMove();
+        NPCInteraction();
     }
 
     private void PlayerBasicMove()
@@ -60,22 +53,29 @@ public class TestPlayerControl : MonoBehaviour
             characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
         }
     }
-    private void PlayerCamMove()
+    private void NPCInteraction()
     {
-        // 마우스 입력 받기
-        yaw += Input.GetAxis("Mouse X") * sensitivity;
-        pitch -= Input.GetAxis("Mouse Y") * sensitivity;
-
-        // 상하 회전 각도 제한
-        pitch = Mathf.Clamp(pitch, -30f, 60f); // -30도에서 60도까지만 제한
-
-        // 카메라 위치 계산
-        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);
-        Vector3 offset = rotation * new Vector3(0f, height, -distance);
-
-        // 카메라 위치와 방향 업데이트
-        Vector3 desiredPosition = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed * Time.deltaTime);
-        transform.LookAt(target.position + Vector3.up * height); // 타겟을 바라봄
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(npcTr != null)
+            {
+                npcTr.GetComponent<NPCStats>().StunAnimPlay();
+            }
+            else
+            {
+                Debug.Log("대상없음");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (npcTr != null)
+            {
+                npcTr.GetComponent<NPCStats>().EscapeStun();
+            }
+            else
+            {
+                Debug.Log("대상없음");
+            }
+        }
     }
 }
