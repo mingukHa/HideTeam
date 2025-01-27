62e3a6151b277eb55c7ed6b38d618c5334ce2f63
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-27 11:06:28 +0900

    no message

--- Assets/HMG/Script/FSM_Modul/NPC.cs
+++ Assets/HMG/Script/FSM_Modul/NPC.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    public string npcName;
+    public string npcName; //NPC의 이름 지정
     public float walkSpeed = 2f;
     public float runSpeed = 5f;
     public bool IsInHurry = false; // 달리기 여부
@@ -11,7 +11,7 @@ public class NPC : MonoBehaviour
     private FSM fsm;
     private Animator animator;
 
-    // Animator Controllers
+    // 에니메이터 추가 하려면 여기서 추가 하면 됨
     public RuntimeAnimatorController walkAnimator;
     public RuntimeAnimatorController runAnimator;
     public RuntimeAnimatorController lookAroundAnimator;
@@ -19,22 +19,22 @@ public class NPC : MonoBehaviour
     private void Start()
     {
         fsm = new FSM(); // FSM 객체 초기화
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); //컴포넌트 받아오기
 
         // 초기 상태 설정
         if (lookAroundAnimator != null)
         {
-            fsm.ChangeState(new LookAroundState(lookAroundAnimator), this);
+            fsm.ChangeState(new LookAroundState(lookAroundAnimator), this); //초기 설정
         }
         else
         {
-            Debug.LogError("LookAroundAnimator가 설정되지 않았습니다.");
+            Debug.LogError("초기 에니메이터가 설정되지 않았습니다.");
         }
     }
 
     private void Update()
     {
-        fsm.Update(this);
+        fsm.Update(this); //상태 업데이트 부분
 
         // 테스트용 입력 처리
         if (Input.GetKeyDown(KeyCode.W))
@@ -53,7 +53,7 @@ public class NPC : MonoBehaviour
             fsm.ChangeState(new WalkState(walkAnimator), this);
         }
     }
-
+    //여기서 변경 됨 이 함수 써야 함
     public void ChangeState(IState newState)
     {
         fsm.ChangeState(newState, this);
@@ -73,9 +73,9 @@ public class NPC : MonoBehaviour
             Debug.Log($"Animator 변경됨: {newAnimator.name}");
         }
     }
-
-    public void Move(float speed)
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
+    //A스타 사용 할거임
+    //public void Move(float speed)
+    //{
+    //    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    //}
 }
