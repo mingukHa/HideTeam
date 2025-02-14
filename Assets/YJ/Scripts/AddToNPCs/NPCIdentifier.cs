94f4c5d759c0c0134caa467799df258991e046dd
Jin <voltagevault@gmail.com>
2025-02-14 10:39:34 +0900

    NPC 애니메이터 걷기, 뛰기 모션 수정

--- Assets/YJ/Scripts/AddToNPCs/NPCIdentifier.cs
+++ Assets/YJ/Scripts/AddToNPCs/NPCIdentifier.cs
@@ -4,8 +4,8 @@ public class NPCIdentifier : MonoBehaviour
 {
     public Avatar nPCAvatar; // 해당 NPC의 아바타
     public string nPCName; // 해당 NPC 이름
-    public string nPCLeftHandIK; // 해당 NPC 왼손 IK 위치
-    public string nPCRightHandIK; // 해당 NPC 오른손 IK 위치
+    //public string nPCLeftHandIK; // 해당 NPC 왼손 IK 위치
+    //public string nPCRightHandIK; // 해당 NPC 오른손 IK 위치
 
     private void Start()
     {
