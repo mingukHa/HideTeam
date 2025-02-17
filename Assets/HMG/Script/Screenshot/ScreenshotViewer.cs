a6355d6b6bdeca26bb9fce69384cfff1186be17c
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-02-17 09:43:12 +0900

    no message

--- Assets/HMG/Script/Screenshot/ScreenshotViewer.cs
+++ Assets/HMG/Script/Screenshot/ScreenshotViewer.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor.Experimental.GraphView;
+using System.Collections.Generic;
 
 public class ScreenshotViewer : MonoBehaviour
 {
     public RawImage displayImage;  // UI에 배치할 RawImage
     public float displayTime = 1.0f;
-
+    public GameObject display;
+    public GameObject Post;
     public void Update()
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
+            Post.SetActive(true);
+            display.SetActive(true);
             StartSlideshow();
         }
     }
@@ -28,10 +34,15 @@ public class ScreenshotViewer : MonoBehaviour
 
     private IEnumerator ShowScreenshots()
     {
-        foreach (var screenshot in ScreenshotManager.Instance.screenshots)
+        List<Texture2D> tempScreenshots = new List<Texture2D>(ScreenshotManager.Instance.screenshots); // 리스트 복사
+
+        for (int i = tempScreenshots.Count; i <= 1; i--)
         {
-            displayImage.texture = screenshot;
+            displayImage.texture = tempScreenshots[i];
             yield return new WaitForSeconds(displayTime);
         }
+
+        SceneManager.LoadScene("MainScene");
     }
+
 }
