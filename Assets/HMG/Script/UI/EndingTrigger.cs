2ed3ab08829ac8d03a700487caa4169a1d6f4f70
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-02-24 16:56:28 +0900

    no message

--- Assets/HMG/Script/UI/EndingTrigger.cs
+++ Assets/HMG/Script/UI/EndingTrigger.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
-
-public class EndingTrigger : MonoBehaviour
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Ending : MonoBehaviour
 {
+    [SerializeField] private Image fadeImage; // 검은색 UI 이미지
+    public float FadeInTime = 2f;
+    public TextMeshProUGUI text;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            EventManager.Trigger(EventManager.GameEventType.Ending);
+            fadeImage.gameObject.SetActive(true);
+            StartCoroutine(FadeIn(FadeInTime));
+        }
+        if (other.CompareTag("NPCEND"))
+        {
+            StartCoroutine(GameOverCoroutine());
         }
     }
+
+
+
+    public IEnumerator FadeIn(float duration)
+    {
+        fadeImage.gameObject.SetActive(true);
+        float elapsedTime = 0f;
+        Color color = fadeImage.color;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+            fadeImage.color = color;
+            yield return null;
+        }
+        FindAnyObjectByType<ESCMenu>().Lobbygame();
+    }
+    public IEnumerator GameOverCoroutine()
+    {
+        text.text = "아무래도 주요인물이 타겟과 접촉한거 같군. 미션 실패야 다시 시작해.";
+        yield return new WaitForSeconds(4.0f);
+        EventManager.Trigger(EventManager.GameEventType.GameOver);
+    }
+
+
 }
